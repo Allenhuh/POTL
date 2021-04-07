852930e47f39910b5db2351c5d960a12fb556398
allenhuh <under-the-ocean@hotmail.com>
2021-04-07 12:23:58 +0800

    app start sh edit

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd //home/ec2-user/server
-npm start
-pm2 start npm --name "potltest" -- start
+pm2 start ./node_modules/react-scripts/scripts/start.js --name "potltest" --watch
 pm2 startup
 pm2 save
 pm2 restart all
