70209cfe3b2d705af5452cb36507501c467b2834
Allenhuh <53265298+Allenhuh@users.noreply.github.com>
2021-05-03 21:01:41 +0800

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,6 @@
 cd /home/ec2-user/server
 npm install
 npm install --save react react-dom react-scripts
+npm install --save reactstrap react react-dom
+npm install --save-dev sass
 npm install pm2 -g
