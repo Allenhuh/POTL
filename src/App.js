0f35d00560a6887252125fe91e4f29938cf1a5d3
Allenhuh <53265298+Allenhuh@users.noreply.github.com>
2021-05-06 16:54:01 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div className="App-header">
         <ul></ul>
         <ul></ul>
-          <text className="app-content-text"> Have you seen them ? </text>
+          <text className="app-content-text"> Have you seen them 6 May ? </text>
         <ul></ul>
         <ul></ul>
       <header className="container-content">
